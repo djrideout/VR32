fa1fb8003d49e9a5d32ca04511e6e6d3904d98e6
D.J. Rideout <djrideoutgit@proton.me>
2023-01-03 21:35:39 -0330

    Fix memory mirroring

--- src/mem.c
+++ src/mem.c
@@ -6,10 +6,9 @@
 unsigned int mem[0x08000000] = {0};
 
 unsigned int mem_read(unsigned int addr) {
+  addr = addr & ~0x7C000000;
   if (addr >= 0x04000000 && addr <= 0x04FFFFFF) {
     return cart[addr & ~0x04000000];
-  } else if (addr >= 0x08000000 && addr <= 0xFFFFFFFF) {
-    return mem[addr & ~0xFF000000];
   } else {
     return mem[addr];
   }
